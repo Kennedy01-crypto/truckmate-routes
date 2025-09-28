fb5cd74500c3f60142e1b348c77455a6382aef33
Jim Kennedy <kennedyomata@gmail.com>
2025-09-28 13:48:46 +0300

    file: constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+import { FileText, MapPin } from "lucide-react";
+
+export const navItems = [
+  {
+    title: "Plan Trip",
+    href: "/plan",
+    icon: MapPin,
+    description: "Plan routes and manage trip details"
+  },
+  {
+    title: "Review Logs",
+    href: "/logs", 
+    icon: FileText,
+    description: "View and manage ELD logs"
+  },
+];
